c4c8bf7cd3f51974f9d0e8b44b6b5d18fd070c45
DaryllGomas <daryll.gomas>
2022-10-31 20:54:33 -0700

    test2

--- PortScanner/portscanner.py
+++ PortScanner/portscanner.py
@@ -9,11 +9,12 @@ print(ascii_banner)
 ip = '10.10.128.157'
 open_ports =[]
 
-port = range(1-65535)
+ports = range(1, 65535)
 
 def probe_port(ip, port, result = 1):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
         r = sock.connect_ex((ip,port))
         if r == 0:
             result = r
@@ -22,7 +23,7 @@ def probe_port(ip, port, result = 1):
         pass
     return result
 
-for ports in port:
+for port in ports:
     sys.stdout.flush()
     response = probe_port(ip, port)
     if response == 0:
@@ -33,4 +34,3 @@ if open_ports:
     print (sorted(open_ports))
 else:
     print ("Looks like no ports are open :(")
-        
