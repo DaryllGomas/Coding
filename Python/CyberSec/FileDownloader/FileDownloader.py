b89f4aad5c8e23ceefa9061f28f1db28c4ec1455
DaryllGomas <111371387+DaryllGomas@users.noreply.github.com>
2022-11-02 06:44:53 -0700

    Update FileDownloader.py

--- Python/CyberSec/FileDownloader/FileDownloader.py
+++ Python/CyberSec/FileDownloader/FileDownloader.py
@@ -3,4 +3,3 @@ import requests
 url = 'https://raw.githubusercontent.com/DaryllGomas/website/master/images/THM-07TDRGKZIH.png'
 r = requests.get(url, allow_redirects=True)
 open('THM-07TDRGKZIH.png', 'wb').write(r.content)
-
