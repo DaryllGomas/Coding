8649dc4ef0c27695ef0a3a8f99e30001963d80d5
DaryllGomas <111371387+DaryllGomas@users.noreply.github.com>
2022-11-29 09:41:36 -0800

    Upload Exercises

---
+++ Python/Mosh/Mosh1/Exercise2.py
@@ -0,0 +1,6 @@
+# Call the input function
+birth_year = input('Enter your birth year: ')
+# pass the str to int
+age = 2022 - int(birth_year)
+#Print Age
+print(age)
---
+++ Python/Mosh/Mosh1/practice1.py
@@ -0,0 +1,3 @@
+print('Hello, what is your name ?')
+name=input()
+print('Hi ' + name + ' ! What can i do for you today !')
