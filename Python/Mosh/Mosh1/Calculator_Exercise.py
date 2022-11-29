2e894aa12831fd72c797ef570cb37a05ef384688
DaryllGomas <111371387+DaryllGomas@users.noreply.github.com>
2022-11-29 10:02:39 -0800

    final commit

---
+++ Python/Mosh/Mosh1/Calculator_Exercise.py
@@ -0,0 +1,10 @@
+#Request for input of the first int or float
+print('Please enter the first number to be calculated')
+#definition of the variable
+first = input()
+#Request for the second int or float  
+print('Please Enter the Second number to be calculated')
+#definition of the variable
+second = input()
+#Passing of str input to int/float variable
+print('Thank you, the sum is ' , int(first) + int(second) )
