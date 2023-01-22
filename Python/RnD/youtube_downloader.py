c95ebb5ec47da5543edaae6cee82e0c1872c2c2f
DaryllGomas <111371387+DaryllGomas@users.noreply.github.com>
2023-01-21 19:36:58 -0800

    downloader commits

--- Python/RnD/youtube_downloader.py
+++ Python/RnD/youtube_downloader.py
@@ -1,5 +1,6 @@
 import os
 import youtube_dl
+import time
 
 # Set the file path to the playlists.txt file
 file_path = "c:\\users\iccanui\desktop\playlists.txt"
@@ -14,14 +15,17 @@ options = {
     'format': 'bestaudio/best',
     'outtmpl': 'C:\\Audio\\%(playlist)s\\%(title)s.%(ext)s',
     'audio-format': 'mp3',
-    'audio-quality': '192'
+    'audio-quality': '192',
 }
+
+# Create a variable to track the number of 403 errors encountered
+errors = 0
 
 # Iterate over the playlists
 for i, playlist in enumerate(playlists):
     if i % 2 == 0: #even number
-        current_playlist = playlist
-        if not current_playlist.startswith("-"):
+        if not playlist.startswith("-"):
+            current_playlist = playlist
             options['outtmpl'] = options['outtmpl'].replace("%(playlist)s", current_playlist)
             # Create the directory if it doesn't exist
             os.makedirs(f"C:\\Audio\\{current_playlist}", exist_ok=True)
@@ -29,5 +33,18 @@ for i, playlist in enumerate(playlists):
         # Make the search on youtube
         query = f"{playlist} {current_playlist} audio"
         url = f"ytsearch1:{query}"
-        with youtube_dl.YoutubeDL(options) as ydl:
-            ydl.download([url])
+        while True:
+            try:
+                with youtube_dl.YoutubeDL(options) as ydl:
+                    ydl.download([url])
+                errors = 0
+                break
+            except youtube_dl.utils.DownloadError as e:
+                if "HTTP Error 403: Forbidden" in str(e):
+                    errors += 1
+                    if errors == 1:
+                        time.sleep(5)
+                    else:
+                        time.sleep(10)
+                else:
+                    raise
