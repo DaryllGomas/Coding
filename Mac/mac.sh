26f364493ad8c70467d07d89ebca985d33a823b2
DaryllGomas <111371387+DaryllGomas@users.noreply.github.com>
2023-03-06 09:15:53 -0800

    code push

--- Mac/mac.sh
+++ Mac/mac.sh
@@ -1,21 +1,9 @@
-#!/bin/bash
 
-# Run daily maintenance script
 sudo periodic daily
-
-# Run weekly maintenance script
 sudo periodic weekly
-
-# Run monthly maintenance script
 sudo periodic monthly
-
-# Clear inactive memory
 sudo purge
-
-# Check and repair file system
 sudo fsck -fy
-
-# Enable TRIM support for third-party SSDs
 sudo trimforce enable
 
 echo "All maintenance scripts completed."
